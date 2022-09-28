23b5c69453312b2e638719490dc2278c9bb3317e
KwonHyeongJin <rnjsgudwls05@naver.com>
2022-09-28 21:33:51 +0900

    4주차_prob_3_if절
    주석 추가

--- 4주차/prob_3_if/main.cpp
+++ 4주차/prob_3_if/main.cpp
@@ -10,25 +10,25 @@ int main(void)
 	printf("Input your arithmetic operation:\n");                                    //Print out "Enter the formula to calculate"
 	scanf_s("%d %c %d", &nA, &arithmeticoperation, 1, &nB);                          // Enter the four-way operation you want to enter
 
-	if (arithmeticoperation == '+')                                             //Determine what the value of the arithmetic symbol is
+	if (arithmeticoperation == '+')                                           
 	{
-		res = nA + nB;
+		res = nA + nB;                                                         //for addition
 		printf("addition: ");
 	}
 	else if (arithmeticoperation == '-')
 	{
-		res = nA - nB;
+		res = nA - nB;                                                         //For Subtraction
 		printf("subtraction: ");
 	}
 	else if (arithmeticoperation == '*')
 	{
-		res = nA * nB;
+		res = nA * nB;                                                         //For multiplication
 		printf("multiplication:");
 	}
 	else
 	{
 		res = nA / nB;
-		printf("division:");
+		printf("division:");                                                   //in other cases
 	}
 	printf("%d %c %d = %d\n", nA, arithmeticoperation, nB, res);
 	system("pause");
